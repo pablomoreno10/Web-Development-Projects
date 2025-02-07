4b84424f8513d8c17a54d18f57b49d25e74137a7
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-02-06 20:53:21 -0500

    Finalize Simon Challenge game full functionality

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,21 +1,38 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 1;
+
+
+function StartGame() {
+  $(document).off('keydown').on("keydown", function() {
+      if (level === 1) {
+        nextSequence();
+      }
+  });
+}
+
+if (level === 1) {
+  StartGame();
+}
 
 function nextSequence() {
+  userClickedPattern = [];
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
+  $("h1").text("Level " + level);
+  level++;
 }
 
-$('button').on("click", function() {
+$('.btn').on("click", function() {
     var userChosenColor = $(this).attr('id');
     userClickedPattern.push(userChosenColor);
-    $("#" + userChosenColor).fadeOut(100).fadeIn(100);
     playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 function playSound(name) {
@@ -28,8 +45,39 @@ function animatePress(currentColor){
     setTimeout(function(){
         $("#" + currentColor).removeClass("pressed");
     }, 100);
+
 }
 
-$(document).on("keydown", function(event) {
-    nextSequence();
-});
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 1;
+    gamePattern = [];
+    userClickedPattern = [];
+    StartGame();
+}
+
+
+
+
+
+
+
+
